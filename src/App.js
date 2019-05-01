81a09e6eff19ebc2c7ddce15d6445012adc46206
Anthony DiRusso <ossurid@gmail.com>
2019-05-01 09:31:12 -0400

    lecture on bind and arrow functions using a basic counter

--- src/App.js
+++ src/App.js
@@ -8,21 +8,59 @@ import {
   Route
   } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/cats">Cats</Link>
-        
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About} />
-        <Route path="/cats" component={Cats} />
-           
-      </header>
-    </div>
-  );
+  //What is bind?
+  // - a way to make sure that a method knows who this is
+  // why do I need bind?
+  //why do I bind?
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+    //in this spot where you def have access to the instance
+    //.bind will lock-in the value of this
+    this._incrementCount = this._incrementCount.bind(this);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h2>{this.state.counter}</h2>
+        <button onClick={this._incrementCount}>increment</button>
+        <h2>{this.state.counter}</h2>
+        <button onClick={this._incrementCountUsingArrow}>increment with Arrow</button>
+        <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/cats">Cats</Link>
+          
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About} />
+          <Route path="/cats" component={Cats} />
+             
+        </header>
+      </div>
+    );
+  }
+  //normal method to use normal functions you have to use the bind method
+  _incrementCount() {
+    console.log('App: _incrementCount');
+    console.log(this);
+    // debugger;
+    this.setState({
+      counter: this.state.counter + 1
+    })
+    
+  }
+  //Arrow functions automatically bind this.
+  _incrementCountUsingArrow = () => {
+    console.log("Arrow Fuction")
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
 }
+
 
 export default App;
